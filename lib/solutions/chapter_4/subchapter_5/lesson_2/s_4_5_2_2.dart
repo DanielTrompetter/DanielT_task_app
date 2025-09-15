e94735f4c86deee33eabc3e6069d7a4478a94638
Daniel Trompetter <danieltrompetter@gmail.com>
2025-09-15 12:19:52 +0200

    solved 4522

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -2,8 +2,30 @@ import 'package:flutter/material.dart';
 
 class S4522 extends StatelessWidget {
   const S4522({super.key});
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const MyStatelessWidget();
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: const [
+          Text('Hello Daniel'), // yeah that's me!
+          SizedBox(height: 12),
+          ElevatedButton(
+            onPressed: null, // Noch kein Klick!!!
+            child: Text('Klick mich'),
+          ),
+        ],
+      ),
+    );
   }
 }
