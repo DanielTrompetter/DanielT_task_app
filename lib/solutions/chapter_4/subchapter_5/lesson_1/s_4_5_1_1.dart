614726d7e7921099106034d2e60871ba74f59bf0
Daniel Trompetter <danieltrompetter@gmail.com>
2025-09-15 12:10:42 +0200

    4511 solved and fixed some issues

--- lib/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1.dart
@@ -4,6 +4,8 @@ class S4511 extends StatelessWidget {
   const S4511({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return const Center(
+      child: Text('Hello World'),
+    );
   }
 }
