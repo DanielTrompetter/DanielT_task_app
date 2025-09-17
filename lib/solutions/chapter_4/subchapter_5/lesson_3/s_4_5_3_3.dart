b782c78c66210dc8e509365a95ea891e29a68724
Daniel Trompetter <danieltrompetter@gmail.com>
2025-09-17 11:40:56 +0200

    solved up to 4535 including

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_3.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 
 class S4533 extends StatelessWidget {
   const S4533({super.key});
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Container(
+      color: Colors.blue,
+      width: 150,
+      height: 150,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center, // vertikal zentriert
+        crossAxisAlignment: CrossAxisAlignment.center, // horizontal zentriert
+        children: const [
+          Text('App'),
+          Text('Akademie')
+        ],
+      ),
+    );
   }
 }
