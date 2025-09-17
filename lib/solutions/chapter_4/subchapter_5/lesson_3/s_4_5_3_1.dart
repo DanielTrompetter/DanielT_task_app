9237e4cad12d05c943eb8f497b595d92124f5fa8
Daniel Trompetter <danieltrompetter@gmail.com>
2025-09-17 10:43:29 +0200

    solved 4531

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
@@ -2,8 +2,28 @@ import 'package:flutter/material.dart';
 
 class S4531 extends StatelessWidget {
   const S4531({super.key});
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Column(
+      children: [
+        MyContainer(color: Colors.blueGrey),
+        MyContainer(color: Colors.blue),
+        MyContainer(color: Colors.cyan),
+      ],
+    );
+  }
+}
+
+class MyContainer extends StatelessWidget {
+  final Color color;
+  const MyContainer({
+    super.key,
+    required this.color,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(width: 100, height: 100, color: color);
   }
 }
