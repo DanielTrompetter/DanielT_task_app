a242617bbaffef7cbd13154d2bbce92d9d370c09
Daniel Trompetter <danieltrompetter@gmail.com>
2025-09-19 08:01:38 +0200

    No idea where this comes from, but it seems i forgot to commit something yesterday!

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
@@ -4,6 +4,34 @@ class S4523 extends StatelessWidget {
   const S4523({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyNameWidget();
+  }
+}
+
+class MyNameWidget extends StatefulWidget {
+  const MyNameWidget({super.key});
+
+  @override
+  createState() => MyNameWidgetState(); // kurz und knapp wegen Melanie. Und den Return-Type brauch ich hier nicht!
+}
+
+class MyNameWidgetState extends State<MyNameWidget> {
+  String name = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(name, style: TextStyle(fontSize: 24)),
+        SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () => setState(() { 
+            name = 'DanielT'; 
+          }),  
+          child: Text('Name anzeigen'),
+        ),
+      ],
+    );
   }
 }
--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -4,6 +4,49 @@ class S4524 extends StatelessWidget {
   const S4524({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ShowHideNameWidget();
+  }
+}
+
+class ShowHideNameWidget extends StatefulWidget {
+  const ShowHideNameWidget({super.key});
+
+  @override
+  createState() => ShowHideNameWidgetState(); // kurz und knapp wegen Melanie. Und den Return-Type brauch ich hier nicht!
+}
+
+class ShowHideNameWidgetState extends State<ShowHideNameWidget> {
+  String name = '';
+  String buttonName = '';
+  final String buttonText1 = 'Name anzeigen';
+  final String buttonText2 = 'Name verstecken';
+
+  @override
+  void initState() {
+    super.initState();
+    buttonName = buttonText1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(name, style: TextStyle(fontSize: 24)),
+        SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () => setState(() {
+            if (buttonName == buttonText1) {
+              name = 'DanielT';
+              buttonName = buttonText2;
+            } else {
+              name = '';
+              buttonName = buttonText1;
+            }
+          }),
+          child: Text(buttonName),
+        ),
+      ],
+    );
   }
 }
