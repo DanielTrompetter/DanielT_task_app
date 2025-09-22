85ffbac662bd18f000d6fea9c97705d32e2da8c1
Daniel Trompetter <danieltrompetter@gmail.com>
2025-09-22 11:22:49 +0200

    solved 4536

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
 
+class Product {
+  final String name;
+  final double price;
+
+  Product({required this.name, required this.price});
+}
+
+final List<Product> products = [
+  Product(name: 'Äpfel', price: 1.20),
+  Product(name: 'Brot', price: 2.50),
+  Product(name: 'Käse', price: 3.80),
+  Product(name: 'Milch', price: 0.99),
+  Product(name: 'Bier', price: 1.29),
+];
+
 class S4536 extends StatelessWidget {
   const S4536({super.key});
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Column(
+      children: [
+        ListView(
+          shrinkWrap: true,
+          children: products.map((product) => ListTile(
+            title: Text('${product.name} – €${product.price.toStringAsFixed(2)}'),
+          )).toList(),
+        ),
+      ],
+    );
   }
 }
